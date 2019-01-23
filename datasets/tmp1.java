162cd0379d3eae802467bc83e7067a39ecfb683e
eumenides <eumescan@qq.com>
2019-01-23 19:36:46 +0800

    my visitor

--- datasets/tmp1.java
+++ datasets/tmp1.java
@@ -1,4 +1,14 @@
 public class Test{
+
+    Test(){
+        super();
+    }
+
+    public static void test(){
+        test();
+        super.test();
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,12 +1,17 @@
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.Name;
+import com.github.javaparser.ast.expr.SuperExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.FileInputStream;
+import java.util.List;
 
 /**
  * @author: eumes
@@ -15,15 +20,28 @@ import java.io.FileInputStream;
 public class Example {
 
     public static void main(String[] args) throws Exception{
-        String fileName = "src/85a827a BrowserProperties.java";
+//        String fileName = "datasets/85a827a BrowserProperties.java";
 
+        String fileName = "datasets/tmp1.java";
         FileInputStream inputStream = new FileInputStream(fileName);
 
         CompilationUnit compilationUnit = JavaParser.parse(inputStream);
 
+//        printNode(compilationUnit.getChildNodes());
+
+
 //        System.out.println(compilationUnit.toString());
 
         compilationUnit.accept(new MethodVisitor(), null);
+    }
+
+    public static void printNode(List<Node> list) {
+        list.forEach(node ->
+            {
+                System.out.println(" |__ " + node.toString());
+                System.out.print("  ");
+                printNode(node.getChildNodes());
+            });
     }
 
     private static class MethodVisitor extends VoidVisitorAdapter<Void> {
@@ -48,6 +66,13 @@ public class Example {
             System.out.println("package: " + n.getName());
             super.visit(n, arg);
         }
+
+        @Override
+        public void visit(MethodCallExpr n, Void arg) {
+            System.out.println("call: " + n.getName());
+            super.visit(n, arg);
+        }
+
 
     }
 
